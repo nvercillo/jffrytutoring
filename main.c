4e28030d8f59f72364c90c9e241ac2880e9f2554
Stefan Vercillo <svercillo7@gmail.com>
2020-11-29 00:50:50 -0500

    Update main.c

--- main.c
+++ main.c
@@ -14,11 +14,14 @@ int main(){
     
     linked_list * ll; 
     ll = malloc(sizeof(linked_list));
-    processsInput(ll);
-    // printList(ll);
+    initLinkedList(ll);
+    searchContinuously(ll);
+    printList(ll);
 
-    // deleteSetence(ll, 1);
+
 
     // free(ll);
     return 0;
 }
+
+
